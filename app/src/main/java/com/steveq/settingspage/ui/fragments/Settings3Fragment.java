5062340a303be5afe60b349f0b656ed5952eb680
SteveQ <sawicki.adam172@gmail.com>
2016-11-17 19:28:02 +0100

    Subscreens and more preferences

---
+++ app/src/main/java/com/steveq/settingspage/ui/fragments/Settings3Fragment.java
@@ -0,0 +1,15 @@
+package com.steveq.settingspage.ui.fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.steveq.settingspage.R;
+
+public class Settings3Fragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences2);
+    }
+}
